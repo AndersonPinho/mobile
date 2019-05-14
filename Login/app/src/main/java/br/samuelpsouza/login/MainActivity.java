7ba6a15962e17e3ef1c497ce000f07203bb123d9
Samuel <samuelsouza.contact@gmail.com>
2019-05-14 10:44:45 -0300

    implementing login request

--- Login/app/src/main/java/br/samuelpsouza/login/MainActivity.java
+++ Login/app/src/main/java/br/samuelpsouza/login/MainActivity.java
@@ -2,6 +2,7 @@ package br.samuelpsouza.login;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void requestLogin(View view) {
+        System.out.println("Trying to login");
     }
 }

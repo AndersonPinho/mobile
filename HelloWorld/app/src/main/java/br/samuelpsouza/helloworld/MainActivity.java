ae7bdbc7c593403e5c873e21bf9c9acff0d0cda2
Samuel <samuelsouza.contact@gmail.com>
2019-05-20 20:20:54 -0300

    calculator first commit

--- HelloWorld/app/src/main/java/br/samuelpsouza/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/br/samuelpsouza/helloworld/MainActivity.java
@@ -1,7 +1,10 @@
 package br.samuelpsouza.helloworld;
 
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.logo);
+        Drawable earthAnimation = imageView.getDrawable();
+        if (earthAnimation instanceof Animatable) {
+            ((Animatable)earthAnimation).start();
+        }
     }
 }
